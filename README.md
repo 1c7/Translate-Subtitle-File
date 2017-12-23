594ef493cfb07e1d1491688deb135bebd0f4fccf
1c7 <guokrfans@gmail.com>
2017-12-23 23:14:54 +0800

    read SRT & parse done

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
-# Electron App
-
-Electron Version: v1.7.9
-
+# Translate Subtitle File(.srt/.ass)
+Electron v1.7.9
 
 ### How to run
 ````
-cd [into this dir]
+cd [into project dir]
 electron .
 ```
 
+### Translate Service Provider: Google
+Translate API there are only Google/Microsort/Yandex
 
-## Resources for Learning Electron
-
-- [electron.atom.io/docs](http://electron.atom.io/docs) - all of Electron's documentation
-- [electron.atom.io/community/#boilerplates](http://electron.atom.io/community/#boilerplates) - sample starter apps created by the community
-- [electron/electron-quick-start](https://github.com/electron/electron-quick-start) - a very basic starter Electron app
-- [electron/simple-samples](https://github.com/electron/simple-samples) - small applications with ideas for taking them further
-- [electron/electron-api-demos](https://github.com/electron/electron-api-demos) - an Electron app that teaches you how to use Electron
-- [hokein/electron-sample-apps](https://github.com/hokein/electron-sample-apps) - small demo apps for the various Electron APIs
+### Google doesn't have free quote:
+see FAQ: https://cloud.google.com/translate/faq?hl=en#pricing
+> Is there any free quota?
+> No, the Google Cloud Translation API is only available as a paid service. Please see Pricing for more details.
--- assets/view.js
+++ assets/view.js
@@ -2,6 +2,13 @@ let $ = require('jquery')  // jQuery now loaded and assigned to $
 const shell = require('electron').shell
 const os = require('os')
 const ipc = require('electron').ipcRenderer
+const fs = require("fs");
+var parser = require('subtitles-parser'); // 解析字幕文件内容
+// const translate = require('google-translate-api'); // 翻译 API
+// not working, no on maintaining
+
+// 保存已提交文件
+var selectedFile = null;
 
 $(function(){
 
@@ -20,14 +27,61 @@ $(function(){
 
   // when file change.
   $(document).on('change', '#file-input', function (event) { 
-    console.log('ha');
+    // console.log('ha');
     
-    var selectedFile = document.getElementById('file-input').files[0];
-    console.log(selectedFile);
+    selectedFile = document.getElementById('file-input').files[0];
+    // console.log(selectedFile);
 
+    // 界面切换
     $('#selected-file-area').show();
     $('.dropzone-wrapper').hide();
   });
 
-})
+  $('#button-area').click(function(){
+    
+    // console.log(selectedFile);
 
+    var root = 'https://jsonplaceholder.typicode.com';
+
+    $.ajax({
+      url: root + '/posts/1',
+      method: 'GET'
+    }).then(function (data) {
+      console.log(data);
+    });
+
+    // fs
+    // var data = fs.readFileSync(selectedFile.path);
+    // console.log("Synchronous read: " + data.toString());
+    // console.log("Program Ended");
+    console.log('click!');
+
+    console.log('asdasd');
+
+    if (selectedFile != null){
+      var srt = fs.readFileSync(selectedFile.path, 'utf8');
+
+      var data = parser.fromSrt(srt);
+      // console.log(data);
+
+      for (var index = 0; index < data.length; index++) {
+        var element = data[index];
+        console.log(element.text);
+        console.log(remove_tag_keep_text(element.text));
+      }
+    }
+
+  })
+
+});
+
+// Remove all tag, but keep text inside tag.
+// Code from: https://stackoverflow.com/questions/5002111/javascript-how-to-strip-html-tags-from-string
+// Input: <font color="#3399CC">Subtitles by </font><font color="ffffff">MemoryOnSmells</font>
+// Output: Subtitles by MemoryOnSmells
+function remove_tag_keep_text(str){
+  var div = document.createElement("div");
+  div.innerHTML = str;
+  var text = div.textContent || div.innerText || "";
+  return text;
+}
