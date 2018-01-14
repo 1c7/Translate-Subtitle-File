93c2bedf8fb7b1d5123ca161fc506f81a42c457d
1c7 <guokrfans@gmail.com>
2018-01-14 13:33:24 +0800

    clean up

--- main.js
+++ main.js
@@ -3,40 +3,24 @@ require('electron-reload')(__dirname);
 const path = require('path')
 const url = require('url')
 
-
-// 保持一个对于 window 对象的全局引用，如果你不这样做，
-// 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
 let win
 
 function createWindow() {
-  // 创建浏览器窗口。
   win = new BrowserWindow({ width: 695, height: 368 })
-
-  // 然后加载应用的 index.html。
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // 打开开发者工具。
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()  // open dev tool
 
-  // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
-    // 与此同时，你应该删除相应的元素。
     win = null
   })
 }
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow)
-
-// 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
   // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
   // 否则绝大部分应用及其菜单栏会保持激活。
@@ -52,6 +36,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-// 在这文件，你可以续写应用剩下主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入。
