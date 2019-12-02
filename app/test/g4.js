44912ebfa87f84badfdcbdcfb8f481edd80c8930
1c7 <guokrfans@gmail.com>
2019-12-02 18:50:29 +0800

    awesome

---
+++ app/test/g4.js
@@ -0,0 +1,88 @@
+const {OAuth2Client} = require('google-auth-library');
+const http = require('http');
+const url = require('url');
+const open = require('open');
+const destroyer = require('server-destroy');
+
+// Download your OAuth2 configuration from the Google
+// const keys = require('./client_secret_1058382469405-cl97ogec0s9an7qantj6dnf5d17uvtfq.apps.googleusercontent.com.json');
+
+/**
+ * Start by acquiring a pre-authenticated oAuth2 client.
+ */
+async function main() {
+  const oAuth2Client = await getAuthenticatedClient();
+  // Make a simple request to the People API using our pre-authenticated client. The `request()` method
+  // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
+  const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
+  const res = await oAuth2Client.request({url});
+  console.log(res.data);
+
+  // After acquiring an access_token, you may want to check on the audience, expiration,
+  // or original scopes requested.  You can do that with the `getTokenInfo` method.
+  const tokenInfo = await oAuth2Client.getTokenInfo(
+    oAuth2Client.credentials.access_token
+  );
+  console.log(tokenInfo);
+}
+
+/**
+ * Create a new OAuth2Client, and go through the OAuth2 content
+ * workflow.  Return the full client to the callback.
+ */
+function getAuthenticatedClient() {
+  return new Promise((resolve, reject) => {
+    // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
+    // which should be downloaded from the Google Developers Console.
+    // const oAuth2Client = new OAuth2Client(
+    //   keys.web.client_id,
+    //   keys.web.client_secret,
+    //   'http://localhost:3000'
+    // );
+
+    const oAuth2Client = new OAuth2Client({
+      clientId: '1058382469405-em0mtqch877g3etc1ddo8j4sk9uafia6.apps.googleusercontent.com',
+      redirectUri: 'http://localhost:3000'
+    });
+
+
+    // Generate the url that will be used for the consent dialog.
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/userinfo.profile',
+    });
+
+    // Open an http server to accept the oauth callback. In this simple example, the
+    // only request to our webserver is to /oauth2callback?code=<code>
+    const server = http
+      .createServer(async (req, res) => {
+        try {
+          if (req.url.indexOf('code') > -1) {
+            // acquire the code from the querystring, and close the web server.
+            const qs = new url.URL(req.url, 'http://localhost:3000')
+              .searchParams;
+            const code = qs.get('code');
+            console.log(`Code is ${code}`);
+            res.end('Authentication successful! Please return to the console.');
+            server.destroy();
+
+            // Now that we have the code, use that to acquire tokens.
+            const r = await oAuth2Client.getToken(code);
+            // Make sure to set the credentials on the OAuth2 client.
+            oAuth2Client.setCredentials(r.tokens);
+            console.info('Tokens acquired.');
+            resolve(oAuth2Client);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .listen(3000, () => {
+        // open the browser to the authorize url to start the workflow
+        open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+      });
+    destroyer(server);
+  });
+}
+
+main().catch(console.error);
